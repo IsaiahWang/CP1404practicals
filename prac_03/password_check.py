2e0a31c114e34446107c8d3626b96ad59f368788
Haidong Wang <haidong.wang@my.jcu.edu.au>
2022-07-20 11:09:33 +0800

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,6 +1,22 @@
 MIN_LENGTH = 8
-password = input('Please input your password: ')
-while len(password) < MIN_LENGTH:
-    print('Too short!')
+
+
+def main():
+    """Function docstring"""
+    password = get_password()
+    while len(password) < MIN_LENGTH:
+        print('Too short!')
+        password = get_password()
+    print_asterisk(password)
+
+
+def print_asterisk(password):
+    print('*' * len(password))
+
+
+def get_password():
     password = input('Please input your password: ')
-print('*' * len(password))
+    return password
+
+
+main()
