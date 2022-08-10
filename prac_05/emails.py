e16a748e546341c02b9ca07d3fb4a8fe51851691
Haidong Wang <haidong.wang@my.jcu.edu.au>
2022-08-10 11:27:03 +0800

    Modify prac5 solutions and add doc strings

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,17 +1,23 @@
+"""
+CP1404 Practical 5
+Email to name dictionary
+"""
+
+
 def main():
-    name_email_dict = {}
+    """Start of program."""
+    email_to_name = {}
     email = input('Email: ')
     while email != '':
         name_str = email.split('@')[0]
         name_list = name_str.split('.')
-        blank = ' '
-        name = blank.join(name_list).title()
-        is_name_correct = input(f'Is your name {name}? (Y/n)').lower()
+        name = ' '.join(name_list).title()
+        is_name_correct = input(f'Is your name {name}? (Y/n) ').lower()
         if is_name_correct != 'y' and is_name_correct != '':
             name = input('Name: ')
-        name_email_dict.update({email:name})
+        email_to_name[email] = name
         email = input('Email: ')
-    for item in name_email_dict.items():
+    for item in email_to_name.items():
         print(f'{item[1]} ({item[0]})')
 
 
