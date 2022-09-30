fcc65dcff4f6c3a0468acd3d6adc496d9dca5587
Haidong Wang <haidong.wang@my.jcu.edu.au>
2022-09-30 13:48:31 +0800

    Finish prac10

---
+++ prac_10/wiki.py
@@ -0,0 +1,21 @@
+import wikipedia
+from wikipedia import DisambiguationError
+
+
+def main():
+    phrase = input('Type in the phrase you want to search: ')
+    while phrase != '':
+        try:
+            page = wikipedia.page(phrase, auto_suggest=False)
+            print('---------title---------')
+            print(page.title)
+            print('--------summary--------')
+            print(page.summary)
+            print('----------URL----------')
+            print(page.url)
+        except DisambiguationError:
+            pass
+        phrase = input('Type in the phrase you want to search: ')
+
+
+main()
